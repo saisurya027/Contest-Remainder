fbb4e2e781178b3d9c515e69e8143184a8350105
saisurya027 <upadrastasaisurya1@gmail.com>
2018-02-14 00:11:13 +0530

    Improve

--- sec.py
+++ sec.py
@@ -6,6 +6,8 @@ lis=[]
 strs=[]
 contest=soup.find(id="contests")
 items=contest.find_all(class_="row contest coming")
+co=0
+comp="4"
 for dat in items:
 	#dat=items[2].find(class_="col-md-5 col-sm-4")
 	into=dat.find(class_="col-md-4 col-sm-6 timeleft countdown")
@@ -20,6 +22,7 @@ for dat in items:
 			ok=1
 	#print(st)
 	c=0
+	#co=0
 	if(ok==1):
 		for i in range(0,len(st)):
 			if(st[i]=='d'):
@@ -30,7 +33,10 @@ for dat in items:
 				st=st[:i+1+c]+":"+st[i+1+c:]
 				c=c+1
 	st="Time Left : "+st
-	print(st)
+	if(ok==0 and st[12]=="0" and ord(st[13])<ord('4')):
+		print(st)
+	else:
+		ok=1
 	item=dat.find(class_="col-md-7 col-sm-8 event")
 #print(item)
 #for line in item.find_all('a'):
@@ -39,5 +45,10 @@ for dat in items:
 	we=web.find('a')
 	t=str(we.get('href'))
 	t="Link      : "+t
-	print(t)
-	print("***************************")
+	if(ok==0):
+		print(t)
+		co=co+1
+		print("***************************")
+if(co==0):
+	print("No Contests!Practice Now")
+	
